c9908aba258dd8ed3547747dfc8947b92d791cf3
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2024-01-23 14:40:24 -0800

    More cleanup.

--- tests/testAssetClipsCSV.py
+++ tests/testAssetClipsCSV.py
@@ -1,5 +1,5 @@
 from fcpxml import AssetClipsCSV
 
-csvWriter = AssetClipsCSV('/Users/gregc/Documents/Code/fcpxml/tests/files/Info.fcpxml')
+csvWriter = AssetClipsCSV('/Users/gregc/Documents/Code/fcpxml/Info.fcpxml')
 success = csvWriter.writeCSV('/Users/gregc/Desktop/Info.csv')
 print(f'success = {success}')
