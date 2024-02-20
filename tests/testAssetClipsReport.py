8560ffc5542b14b67968b958d9b0ce0a8c80379b
Greg Chapman <75333244+gregchapman-dev@users.noreply.github.com>
2024-02-20 14:25:00 -0800

    Update test.

--- tests/testAssetClipsReport.py
+++ tests/testAssetClipsReport.py
@@ -1,5 +1,5 @@
 from fcpxml import AssetClipsReport
 
-reportWriter = AssetClipsReport('/Users/gregc/Documents/Code/fcpxml/Info_19feb2024.fcpxml')
-success = reportWriter.writeReport('/Users/gregc/Desktop/Info_19feb2024.txt')
+reportWriter = AssetClipsReport('/Users/gregc/Documents/Code/fcpxml/Info_20Feb2024.fcpxml')
+success = reportWriter.writeReport('/Users/gregc/Desktop/Info_20Feb2024.txt')
 print(f'success = {success}')
